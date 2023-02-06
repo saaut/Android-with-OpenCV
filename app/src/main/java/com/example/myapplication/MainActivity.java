3a7378df72a51101233e8bef3a961c4563116e4d
saaut <saaut60@naver.com>
2023-02-06 17:01:35 +0900

    part5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.myapplication;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
@@ -16,13 +15,24 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
 import org.opencv.imgproc.Imgproc;
-
+import org.opencv.utils.Converters;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
 import static android.Manifest.permission.CAMERA;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
 
 
 public class MainActivity extends AppCompatActivity
@@ -31,9 +41,12 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "opencv";
     private Mat matInput;
     private Mat matResult;
-    boolean startCanny = false;
+    boolean startYolo = false;
+    boolean firstTimeYolo = false;
+    Net tinyYolo;
 
-    private CameraBridgeViewBase cameraBridgeViewBase;
+
+    CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
 
     public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
@@ -43,15 +56,22 @@ public class MainActivity extends AppCompatActivity
         System.loadLibrary("opencv_java4");
         System.loadLibrary("native-lib");
     }
+    public void YOLO(View Button){
 
-    public void Canny(View Button){
+        if (startYolo == false){
 
-        if (startCanny == false){
-            startCanny = true;
+            startYolo = true;
+
+            if (firstTimeYolo == false){
+                firstTimeYolo = true;
+                String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
+                String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+
+                tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+            }
         }
         else{
-            startCanny = false;
-
+            startYolo = false;
         }
     }
 
@@ -104,10 +124,7 @@ public class MainActivity extends AppCompatActivity
                         super.onManagerConnected(status);
                         break;
                 }
-
-
             }
-
         };
     }
 
@@ -144,6 +161,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onCameraViewStarted(int width, int height) {
 
+        if (startYolo == true){
+
+            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg" ;
+            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+
+            tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+
+
+        }
     }
 
     @Override
@@ -156,15 +182,123 @@ public class MainActivity extends AppCompatActivity
 
         Mat frame = inputFrame.rgba();
 
-        if (startCanny == true) {
+        if (startYolo == true) {
 
-            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
-            Imgproc.Canny(frame, frame, 100, 80);
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
 
+
+
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416,416),new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
+
+
+            tinyYolo.setInput(imageBlob);
+
+
+
+            java.util.List<Mat> result = new java.util.ArrayList<Mat>(2);
+
+            List<String> outBlobNames = new java.util.ArrayList<>();
+            outBlobNames.add(0, "yolo_16");
+            outBlobNames.add(1, "yolo_23");
+
+            tinyYolo.forward(result,outBlobNames);
+
+
+            float confThreshold = 0.3f;
+
+
+
+            List<Integer> clsIds = new ArrayList<>();
+            List<Float> confs = new ArrayList<>();
+            List<Rect> rects = new ArrayList<>();
+
+
+
+
+            for (int i = 0; i < result.size(); ++i)
+            {
+
+                Mat level = result.get(i);
+
+                for (int j = 0; j < level.rows(); ++j)
+                {
+                    Mat row = level.row(j);
+                    Mat scores = row.colRange(5, level.cols());
+
+                    Core.MinMaxLocResult mm = Core.minMaxLoc(scores);
+
+
+
+
+                    float confidence = (float)mm.maxVal;
+
+
+                    Point classIdPoint = mm.maxLoc;
+
+
+
+                    if (confidence > confThreshold)
+                    {
+                        int centerX = (int)(row.get(0,0)[0] * frame.cols());
+                        int centerY = (int)(row.get(0,1)[0] * frame.rows());
+                        int width   = (int)(row.get(0,2)[0] * frame.cols());
+                        int height  = (int)(row.get(0,3)[0] * frame.rows());
+
+
+                        int left    = centerX - width  / 2;
+                        int top     = centerY - height / 2;
+
+                        clsIds.add((int)classIdPoint.x);
+                        confs.add((float)confidence);
+
+                        rects.add(new Rect(left, top, width, height));
+                    }
+                }
+            }
+            int ArrayLength = confs.size();
+
+            if (ArrayLength>=1) {
+                // Apply non-maximum suppression procedure.
+                float nmsThresh = 0.2f;
+
+
+                MatOfFloat confidences = new MatOfFloat(Converters.vector_float_to_Mat(confs));
+
+
+                Rect[] boxesArray = rects.toArray(new Rect[0]);
+
+                MatOfRect boxes = new MatOfRect(boxesArray);
+
+                MatOfInt indices = new MatOfInt();
+
+
+                Dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThresh, indices);
+
+                // Draw result boxes:
+                int[] ind = indices.toArray();
+                for (int i = 0; i < ind.length; ++i) {
+
+                    int idx = ind[i];
+                    Rect box = boxesArray[idx];
+
+                    int idGuy = clsIds.get(idx);
+
+                    float conf = confs.get(idx);
+
+
+                    List<String> cocoNames = Arrays.asList("a person", "a bicycle", "a motorbike", "an airplane", "a bus", "a train", "a truck", "a boat", "a traffic light", "a fire hydrant", "a stop sign", "a parking meter", "a car", "a bench", "a bird", "a cat", "a dog", "a horse", "a sheep", "a cow", "an elephant", "a bear", "a zebra", "a giraffe", "a backpack", "an umbrella", "a handbag", "a tie", "a suitcase", "a frisbee", "skis", "a snowboard", "a sports ball", "a kite", "a baseball bat", "a baseball glove", "a skateboard", "a surfboard", "a tennis racket", "a bottle", "a wine glass", "a cup", "a fork", "a knife", "a spoon", "a bowl", "a banana", "an apple", "a sandwich", "an orange", "broccoli", "a carrot", "a hot dog", "a pizza", "a doughnut", "a cake", "a chair", "a sofa", "a potted plant", "a bed", "a dining table", "a toilet", "a TV monitor", "a laptop", "a computer mouse", "a remote control", "a keyboard", "a cell phone", "a microwave", "an oven", "a toaster", "a sink", "a refrigerator", "a book", "a clock", "a vase", "a pair of scissors", "a teddy bear", "a hair drier", "a toothbrush");
+
+
+                    int intConf = (int) (conf * 100);
+
+                    Imgproc.putText(frame,cocoNames.get(idGuy) + " " + intConf + "%",box.tl(),Imgproc.FONT_HERSHEY_SIMPLEX
+                            , 2, new Scalar(255,255,0),2);
+
+                    Imgproc.rectangle(frame, box.tl(), box.br(), new Scalar(255, 0, 0), 2);
+
+                }
+            }
         }
-
-
-
         return frame;
     }
 
